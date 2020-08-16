b5de3c7fed76f54666e89eee6e88c348dea16f66
jude-afam <afamjudeo@gmail.com>
2020-08-16 15:01:36 +0100

    implemented user login integration

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
-import React from 'react';
-import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
-import SignUp from './components/SignUp';
-import Home from './components/Home';
-import Feedback from './components/Feedback';
-import LogIn from './components/LogIn';
-import Blog from './components/Blog';
+import React from "react";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import SignUp from "./components/SignUp";
+import Home from "./components/Home";
+import Feedback from "./components/Feedback";
+import LogIn from "./components/LogIn";
+import Blog from "./components/Blog";
+import Pending from "./components/Pending";
 
 function App() {
   return (
@@ -13,13 +14,13 @@ function App() {
         {/* <SignUp /> */}
         {/* <Home /> */}
         <Switch>
-          <Route exact path='/' component={LogIn} />
-          <Route path='/home' component={Home} />
-          <Route path='/feedback' component={Feedback} />
-          <Route path='/signup' component={SignUp} />
-          <Route path='/blog' component={Blog} />
+          <Route exact path="/" component={LogIn} />
+          <Route path="/home" component={Home} />
+          <Route path="/feedback" component={Feedback} />
+          <Route path="/signup" component={SignUp} />
+          <Route path="/blog" component={Blog} />
+          <Route path="/pending" component={Pending} />
         </Switch>
-
       </Router>
     </div>
   );
