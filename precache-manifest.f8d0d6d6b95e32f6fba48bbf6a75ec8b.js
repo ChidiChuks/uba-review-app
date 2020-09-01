3b07979a1da2d00a2f94729fed9a8cadce7a07b6
ChidiChuks <chukschiboy@gmail.com>
2020-09-01 10:29:34 -0700

    Updates

---
+++ precache-manifest.f8d0d6d6b95e32f6fba48bbf6a75ec8b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "72d594fad287742655a8f41305d36845",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b0d6f8a234e2ac0b7ce7",
+    "url": "/static/css/main.69eda509.chunk.css"
+  },
+  {
+    "revision": "7e87bc3b700fb14e92ee",
+    "url": "/static/js/2.04b0bb2a.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.04b0bb2a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b0d6f8a234e2ac0b7ce7",
+    "url": "/static/js/main.828eb0f8.chunk.js"
+  },
+  {
+    "revision": "a533602362560694d509",
+    "url": "/static/js/runtime-main.28273911.js"
+  }
+]);
