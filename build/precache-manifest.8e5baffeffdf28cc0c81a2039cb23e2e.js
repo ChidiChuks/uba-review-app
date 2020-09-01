0b1067a24772c8b24583b73cbb73ae054d0e50aa
ChidiChuks <chukschiboy@gmail.com>
2020-09-01 02:04:24 -0700

    update on the build project for production

---
+++ build/precache-manifest.8e5baffeffdf28cc0c81a2039cb23e2e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "411bf1e1aa8db31705fc4f3cf9e113bc",
+    "url": "/index.html"
+  },
+  {
+    "revision": "489aa789e944cd766783",
+    "url": "/static/css/main.69eda509.chunk.css"
+  },
+  {
+    "revision": "4da155a246d6f6363f62",
+    "url": "/static/js/2.58a45a21.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.58a45a21.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "489aa789e944cd766783",
+    "url": "/static/js/main.1f135a9c.chunk.js"
+  },
+  {
+    "revision": "a533602362560694d509",
+    "url": "/static/js/runtime-main.28273911.js"
+  }
+]);
